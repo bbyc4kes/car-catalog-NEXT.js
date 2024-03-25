0f668ced643b0e08f7edab53d78af94ca9944810
Max <azimov.workspace@gmail.com>
2024-03-25 18:57:04 +0100

    adding cardetailsprops interface

--- types/index.tsx
+++ types/index.tsx
@@ -5,9 +5,33 @@ export interface CustomButtonProps {
   containerStyles?: string
   btnType?: 'button' | 'submit'
   handleClick?: MouseEventHandler<HTMLButtonElement>
+  rightIcon?: string
+  textStyles?: string
+  isDisabled?: boolean
 }
 
 export interface searManufacturerProps {
   manufacturer?: string
   setManufacturer?: (manufacturer: string) => void
+}
+
+export interface CarProps {
+  city_mpg: number
+  class: string
+  combination_mpg: number
+  cylinders: number
+  displacement: number
+  drive: string
+  fuel_type: string
+  highway_mpg: number
+  make: string
+  model: string
+  transmission: string
+  year: number
+}
+
+export interface CarDetailsProps {
+  isOpen: boolean
+  closeModal: () => void
+  car: CarProps
 }
