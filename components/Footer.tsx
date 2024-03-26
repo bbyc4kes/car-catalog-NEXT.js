64798cbaafae0199592e6177aeb7fb841bc7f9b2
Max <azimov.workspace@gmail.com>
2024-03-26 20:43:43 +0100

    edited app logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-black-100 mt-5 broder-t border-gray-100">
+    <footer className="flex flex-col text-black-100 mt-5 broder-t border-gray-100 container mx-auto">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
@@ -15,10 +15,7 @@ const Footer = () => {
             height={18}
             className="object-contain"
           />
-          <p className="text-base text-gray-700">
-            Auto Hub 2024 <br />
-            All rights reserved &copy;
-          </p>
+          <p className="text-base text-gray-700">Auto Hub 2024 &copy;</p>
         </div>
 
         <div className="footer__links">
@@ -26,7 +23,11 @@ const Footer = () => {
             <div key={l.title} className="footer__link">
               <h3 className="font-bold">{l.title}</h3>
               {l.links.map((i) => (
-                <Link key={i.title} href={i.url} className="text-gray-500">
+                <Link
+                  key={i.title}
+                  href={i.url}
+                  className="text-gray-500 hover:text-gray-600 duration-200"
+                >
                   {i.title}
                 </Link>
               ))}
@@ -36,7 +37,9 @@ const Footer = () => {
       </div>
 
       <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-        <p>&copy; AutoHub. All Rights Reserved</p>
+        <div className="flex-1 flex">
+          <p>&copy; AutoHub. All Rights Reserved</p>
+        </div>
         <div className="footer__copyrights-link">
           <Link href="/" className="text-gray-500">
             Privacy Policy
