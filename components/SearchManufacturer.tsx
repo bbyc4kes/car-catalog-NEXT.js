0bc8ecfd8b11f220bb61347b36b70507b1e364e0
Max <azimov.workspace@gmail.com>
2024-03-26 20:45:45 +0100

    handling filtering

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -36,62 +36,70 @@ const SearchManufacturer = ({
               className="ml-4"
             />
           </Combobox.Button>
-          <Combobox.Input
-            className={'search-manufacturer__input'}
-            placeholder="Volkswagen"
-            displayValue={(manufacturer: string) => manufacturer}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            afterLeave={() => setQuery('')}
-          >
-            <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== '' ? (
-                <Combobox.Option
-                  value={query}
-                  className={
-                    'search-manufacturer__option relative cursor-default select-none'
-                  }
-                >
-                  Nothing found.
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active }) =>
-                      `relative search-manufacturer__option ${
-                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
-                      }`
-                    }
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}
-                        >
-                          {item}
-                        </span>
-                        {selected && (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-gray-700'
-                            }`}
-                          ></span>
+          <div className="relative">
+            <Combobox.Input
+              className={
+                'search-manufacturer__input hover:shadow-lg duration-500'
+              }
+              placeholder="Volkswagen"
+              displayValue={(manufacturer: string) => manufacturer}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <div className="absolute z-20 w-full mt-1 bg-white  rounded-md shadow-lg">
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                afterLeave={() => setQuery('')}
+              >
+                <Combobox.Options>
+                  {filteredManufacturers.length === 0 && query !== '' ? (
+                    <Combobox.Option
+                      value={query}
+                      className={
+                        'search-manufacturer__option relative cursor-default select-none'
+                      }
+                    >
+                      Nothing found.
+                    </Combobox.Option>
+                  ) : (
+                    filteredManufacturers.map((item) => (
+                      <Combobox.Option
+                        key={item}
+                        className={({ active }) =>
+                          `relative search-manufacturer__option ${
+                            active
+                              ? 'bg-primary-blue text-white'
+                              : 'text-gray-900'
+                          }`
+                        }
+                        value={item}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
+                              }`}
+                            >
+                              {item}
+                            </span>
+                            {selected && (
+                              <span
+                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                  active ? 'text-white' : 'text-gray-700'
+                                }`}
+                              ></span>
+                            )}
+                          </>
                         )}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
+                      </Combobox.Option>
+                    ))
+                  )}
+                </Combobox.Options>
+              </Transition>
+            </div>
+          </div>
         </div>
       </Combobox>
     </div>
