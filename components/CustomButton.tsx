2993967106112c7adcf4298f409a3c4f1ec0c118
Max <azimov.workspace@gmail.com>
2024-03-26 20:42:26 +0100

    additional props type management

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,22 +1,36 @@
 'use client'
 
 import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
 import React from 'react'
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  textStyles,
+  rightIcon,
+  isDisabled,
   btnType,
 }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled || false}
       type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            className="object-contain"
+            fill
+          />
+        </div>
+      )}
     </button>
   )
 }
